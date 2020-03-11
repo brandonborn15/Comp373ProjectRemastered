972c766d6c8f047ffc9182c390f4ece7c0694181
Thomas Walsh <lwalsh7@luc.edu>
2020-03-11 13:03:36 -0500

    Create FacilityManagementClient.java

---
+++ src/client/FacilityManagementClient.java
@@ -0,0 +1,12 @@
+package src.client;
+
+import java.util.List;
+
+import src.facility.*;
+import src.facility_maintenance.*;
+import src.facility_management.*;
+import src.facility_use.*;
+
+public class FacilityManagementClient{
+
+}
