ca26ea66b9135a7ddd66b9f3ac6129f5639016b0
Thomas Walsh <lwalsh7@luc.edu>
2020-03-11 00:33:17 -0500

    Facility and FacilityImpl

--- src/facility/Facility.java
+++ src/facility/Facility.java
@@ -1,5 +1,16 @@
 package src.facility;
 
+import java.util.List;
+
+import src.facility_use.FacilityUse;
+import src.facility_maintenance.FacilityMaintenance;
 public interface Facility {
     
+    public int requestAvailableCapacity(int numAtendees);
+    public void addFacilityDetail();
+    public List<FacilityUse> getFacilityUses();
+    public void setFacilityUses(List<FacilityUse> facilityUses);
+    public List<FacilityMaintenance> getFacilityMaints();
+    public void setFacilityMaints(List<FacilityMaintenance> facilityMaints);
+
 }
--- src/facility/FacilityImpl.java
+++ src/facility/FacilityImpl.java
@@ -1,5 +1,55 @@
 package src.facility;
 
+import java.util.List;
+
+import src.facility_use.FacilityUse;
+import src.facility_maintenance.FacilityMaintenance;
+
 public class FacilityImpl implements Facility {
+    public List<Facility> facilities;
+    private String facilityName;
+    private int facilityCapacity;
+    private List<FacilityUse> facilityUses;
+    private List<FacilityMaintenance> facilityMaints;
+
+    public int requestAvailableCapacity(int numAtendees){
+        int availableCapacity = facilityCapacity - numAtendees;
+        return availableCapacity;
+    }
     
+    public void addFacilityDetail(){
+
+    }
+
+    public String getFacilityName(){
+        return facilityName;
+    }
+
+    public void setFacilityName(String facilityName){
+        this.facilityName = facilityName;
+    }
+
+    public int getFacilityCapacity(){
+        return facilityCapacity;
+    }
+
+    public void setFacilityCapacity(int facilityCapacity){
+        this.facilityCapacity = facilityCapacity;
+    }
+
+    public List<FacilityUse> getFacilityUses(){
+        return facilityUses;
+    }
+
+    public void setFacilityUses(List<FacilityUse> facilityUses){
+        this.facilityUses = facilityUses;
+    }
+
+    public List<FacilityMaintenance> getFacilityMaints(){
+        return facilityMaints;
+    }
+
+    public void setFacilityMaints(List<FacilityMaintenance> facilityMaints){
+        this.facilityMaints = facilityMaints;
+    }
 }
