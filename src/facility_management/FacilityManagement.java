0b91796407e0abb61b7baf71fed8f6152919e66f
brandon <btm1998x@gmail.com>
2020-03-11 17:51:51 -0500

    Update FacilityManagement.java

--- src/facility_management/FacilityManagement.java
+++ src/facility_management/FacilityManagement.java
@@ -8,7 +8,7 @@ public interface FacilityManagement {
     
     public List<Facility> getFacilities();
     public void setFacilities(List<Facility> facilities);
-    public void listFacilities();
+    public String listFacilities();
     public void addNewFacility(String name, Integer capacity);
     public void removeFacility(Facility facility);
     public void getFacilityInformation(Facility facility);
