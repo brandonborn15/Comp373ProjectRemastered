e5a6a05fefdc17a81bbaa38b66e85ecd0b060e31
brandon <btm1998x@gmail.com>
2020-03-11 16:18:30 -0500

    Update FacilityUse.java

--- src/facility_use/FacilityUse.java
+++ src/facility_use/FacilityUse.java
@@ -6,8 +6,10 @@ public interface FacilityUse {
     
     public List<GeneralUse> getReservations();
     public void setReservations(List<GeneralUse> reservations);
+
     public List<Inspections> getInspections();
     public void setInspections(List<Inspections> inspections);
+    
     public boolean isInUseDuringInterval(String targetDate);
     public void vacateFacility(String vacateDate);
 
