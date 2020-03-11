022d393a16bf125408d649b665665094f3190c7d
brandon <btm1998x@gmail.com>
2020-03-11 16:18:27 -0500

    Update FacilityManagementImpl.java

--- src/facility_management/FacilityManagementImpl.java
+++ src/facility_management/FacilityManagementImpl.java
@@ -1,13 +1,16 @@
 package src.facility_management;
 
 import src.facility.Facility;
+import src.facility.FacilityImpl;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class FacilityManagementImpl implements FacilityManagement{
-    public List<Facility> facilities;
+    private List<Facility> facilities;
 
     public List<Facility> getFacilities(){
+        //System.out.println(facilities);
         return facilities;
     }
 
@@ -16,15 +19,20 @@ public class FacilityManagementImpl implements FacilityManagement{
     }
 
     public void listFacilities(){
-
+        System.out.println(this.facilities);
     }
 
-    public void addNewFacility(Facility facility){
+    public void addNewFacility(String name, Integer capacity){
+        //List<Facility> facility = new LinkedList<Facility>();
+        FacilityImpl facility = new FacilityImpl();
+        facility.setFacilityName(name);
+        facility.setFacilityCapacity(capacity);
 
+        this.facilities.add(facility);
     }
 
     public void removeFacility(Facility facility){
-
+        this.facilities.remove(facility);
     }
 
     public void getFacilityInformation(Facility facility){
