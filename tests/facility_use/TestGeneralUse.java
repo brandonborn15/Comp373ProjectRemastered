eeaa1344d65604bcf338b77f69f1e020bb075da1
Thomas Walsh <lwalsh7@luc.edu>
2020-03-15 11:48:10 -0500

    Update TestGeneralUse.java

--- tests/facility_use/TestGeneralUse.java
+++ tests/facility_use/TestGeneralUse.java
@@ -1,12 +1,17 @@
 package tests.facility_use;
 
-import src.facility_use.GeneralUse;
+import src.facility_use.GeneralUseImpl;
 
 public class TestGeneralUse{
     
-    //Test getter and setter for resDate
-    //Test assignFacilityToUse
-    //Test listActualUsage
-    //Test calcUsageRate
-    
+    public void testGenUse(){
+        GeneralUseImpl genUse1 = new GeneralUseImpl();
+        genUse1.setResDate("03/30/2020");
+        String test = genUse1.getResDate();
+        if (test.equals("03/30/2020")){
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+    }
 }
