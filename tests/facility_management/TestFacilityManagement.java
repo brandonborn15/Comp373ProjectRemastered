65b153125a3d03e4986d707de1fe01eedc3bfa66
Thomas Walsh <lwalsh7@luc.edu>
2020-03-15 14:15:43 -0500

    Update TestFacilityManagement.java

--- tests/facility_management/TestFacilityManagement.java
+++ tests/facility_management/TestFacilityManagement.java
@@ -4,10 +4,30 @@ import src.facility_management.FacilityManagement;
 
 public class TestFacilityManagement{
 
-    //Test getter and setter for facilities
-    //Test listFacilities
-    //Test addFacility
+    public void testFacilityManagement(){
+        LinkedList<FacilityImpl> facilities = new LinkedList<FacilityImpl>();
+        LinkedList<String test = new LinkedList<String>();
+        LinkedList<FacilityImpl> test1 = new LinkedList<FacilityImpl>();
+        FacilityImpl fac = new FacilityImpl();
+        fac.setFacilityName("Test");
+        FacilityManagementImpl fM = new FacilityManagementImpl();
+        facilities.add(facility);
+        fM.setFacilities(facilities);
+        test = fM.getFacilities();
+        if (test == facilities){
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+        test1 = fM.listFacilities();
+        if (test1 == ["Test"]){
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+    }
+    //Test addNewFacility
     //Test removeFacility
     //Test getFacilityInformation
-    
+    //Test assignFacilityToUse
 }
