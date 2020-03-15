71ae72cf8d8017b517a76b306e79e42d871d9eeb
Thomas Walsh <lwalsh7@luc.edu>
2020-03-15 14:05:33 -0500

    Update TestFacilityUse.java

--- tests/facility_use/TestFacilityUse.java
+++ tests/facility_use/TestFacilityUse.java
@@ -5,19 +5,37 @@ import java.util.LinkedList;
 import src.facility_use.*;
 
 public class TestFacilityUse{
-    LinkedList<InspectionImpl> inspections = new LinkedList<>();
-    LinkedList<GeneralUseImpl> reservations = new LinkedList<>();
-    LinkedList<InspectionImpl> test1 = new LinkedList<InspectionImpl>();
-    LinkedList<GeneralUseImpl> test2 = new LinkedList<GeneralUseImpl>();
-    InspectionImpl inspec = new InspectionImpl();
-    GeneralUseImpl gU = new GeneralUseImpl();
-    FacilityUseImpl fU = new FacilityUseImpl();
-    inspections.add(inspec);
-    reservations.add(gU);
-    fU.setReservations(reservations);
-    fU.setInspections(inspections);
-    
-    //Test isInUseDuringInterval
-    //Test vacateFacility
-    
+
+    public void testFacilityUse(){
+        LinkedList<InspectionImpl> inspections = new LinkedList<>();
+        LinkedList<GeneralUseImpl> reservations = new LinkedList<>();
+        LinkedList<InspectionImpl> test1 = new LinkedList<InspectionImpl>();
+        LinkedList<GeneralUseImpl> test2 = new LinkedList<GeneralUseImpl>();
+        InspectionImpl inspec = new InspectionImpl();
+        GeneralUseImpl gU = new GeneralUseImpl();
+        FacilityUseImpl fU = new FacilityUseImpl();
+        inspections.add(inspec);
+        reservations.add(gU);
+        fU.setReservations(reservations);
+        fU.setInspections(inspections);
+        test1 = fU.getInpections();
+        test2 = fU.getReservations();
+        if (test1 == inspections){
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+        if (test2 == reservations){
+            System.out.println("Passed");
+        } else{
+            System.out.println("Failed");
+        }
+        //Test addReservation
+        //Test scheduleInspection
+        //Test isInUseDuringInterval
+        //Test vacateFacility
+        //Test listInspections
+        //Test calcUsageRate
+        //Test listActualUsage
+    }  
 }
