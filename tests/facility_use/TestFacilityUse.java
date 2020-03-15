9c27ca30c4f7b714cf436759e26174bc092be69d
Thomas Walsh <lwalsh7@luc.edu>
2020-03-15 13:25:28 -0500

    Update TestFacilityUse.java

--- tests/facility_use/TestFacilityUse.java
+++ tests/facility_use/TestFacilityUse.java
@@ -3,7 +3,7 @@ package tests.facility_use;
 import src.facility_use.*;
 
 public class TestFacilityUse{
-   /* LinkedList<InspectionImpl> inspections = new LinkedList<>();
+    LinkedList<InspectionImpl> inspections = new LinkedList<>();
     LinkedList<GeneralUseImpl> reservations = new LinkedList<>();
     LinkedList<InspectionImpl> test1 = new LinkedList<InspectionImpl>();
     LinkedList<GeneralUseImpl test2 = new LinkedList<GeneralUseImpl>();
